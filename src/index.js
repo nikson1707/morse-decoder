dc1e7bb19190b3c5f0dce84e4f1a558cd24c24c4
Devil-mYcRy <51002209+Devil-mYcRy@users.noreply.github.com>
2020-03-06 19:33:00 +0300

    feat: finish work

--- src/index.js
+++ src/index.js
@@ -35,10 +35,48 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+    ' ': ''
 };
 
 function decode(expr) {
     // write your solution here
+        const message =[];
+        let temp = '';
+        let res = '';
+        for(let i = 0; i != expr.length;i+=10){
+            let temp2 ='';
+            temp = expr.substring(i, i+10);
+            if(temp ==='**********'){
+                temp2+=' ';            
+            }
+            temp.split('');
+            for(let j = 0; j!= temp.length;j+=2){
+                if(temp[j]==='1' && temp[j+1]==='0'){
+                    temp2+='.';
+                }
+                if(temp[j]==='1' && temp[j+1]==='1'){
+                    temp2+='-';
+                }
+                else{
+                    temp2+='';
+                }
+            }
+            message.push(temp2)
+        }
+        for(let i = 0;i != message.length;++i)
+        {
+            for (let [key, value] of Object.entries(MORSE_TABLE)) {
+                if(message[i] === key){
+                    res += `${value}`;
+                }
+                
+            }
+            if(message[i] === ' '){
+                    res += ' ';
+            }
+
+        }
+        return res;
 }
 
 module.exports = {
